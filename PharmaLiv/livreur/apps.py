069d3f72cedbb3bbc65570ccc700f5b8931ba0f5
birametgod <birametgod95@gmail.com>
2018-03-28 15:31:02 +0000

    adding files

---
+++ PharmaLiv/livreur/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LivreurConfig(AppConfig):
+    name = 'livreur'
---
+++ PharmaLiv/payementLigne/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PayementligneConfig(AppConfig):
+    name = 'payementLigne'
