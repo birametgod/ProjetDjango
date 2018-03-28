7bf781ffdb05e1bd7610d73c46b5c9de8f57d1e1
birametgod <birametgod95@gmail.com>
2018-03-28 20:11:24 +0000

    adding files

--- PharmaLiv/patient/models.py
+++ PharmaLiv/patient/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    login = models.CharField(max_length=30,null=True)  #Le paramètre null, lorsque mis à True, indique à Django que ce champ peut être laissé vide et qu'il est donc optionnel.  
+    password = models.CharField(max_length=30,null=True)
+    nom = models.CharField(max_length=30, null = True)
+    prenom = models.CharField(max_length=30, null = True)
+    sexe = models.CharField(max_length=30, null = True)
+    allergie = models.CharField(max_length=100, null = True)
+    traitement = models.CharField(max_length=100,null=True)
+    dateNaissance = models.DateTimeField(auto_now_add=True, auto_now=False,
+    verbose_name="Date de naissance")
+    
+    def __str__(self):
+        """
+        Cette méthode que nous définirons dans tous les modèles
+        nous permettra de reconnaître facilement les différents objets que 
+        nous traiterons plus tard dans l'administration
+        """
+        return self.nom 
+
+    def __unicode__(self):
+        return 
