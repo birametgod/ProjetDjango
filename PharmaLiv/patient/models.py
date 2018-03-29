7faadba6ec6ca229daebce35d39d3f6fe89a01b5
birametgod <birametgod95@gmail.com>
2018-03-29 12:17:42 +0000

    modification model

--- PharmaLiv/patient/models.py
+++ PharmaLiv/patient/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Patient(models.Model):
-    login = models.CharField(max_length=30,null=True)  #Le paramètre null, lorsque mis à True, indique à Django que ce champ peut être laissé vide et qu'il est donc optionnel.  
-    password = models.CharField(max_length=30,null=True)
-    nom = models.CharField(max_length=30, null = True)
-    prenom = models.CharField(max_length=30, null = True)
-    sexe = models.CharField(max_length=30, null = True)
-    allergie = models.CharField(max_length=100, null = True)
-    traitement = models.CharField(max_length=100,null=True)
+    login = models.CharField(max_length=30,null=False,unique=True)  #Le paramètre null, lorsque mis à True, indique à Django que ce champ peut être laissé vide et qu'il est donc optionnel.  
+    password = models.CharField(max_length=30,null=False)
+    nom = models.CharField(max_length=30, null = False)
+    prenom = models.CharField(max_length=30, null = False)
+    sexe = models.CharField(max_length=30, null = False)
+    allergie = models.CharField(max_length=100, null = False)
+    traitement = models.CharField(max_length=100,null=False)
     dateNaissance = models.DateTimeField(auto_now_add=True, auto_now=False,
     verbose_name="Date de naissance")
     
