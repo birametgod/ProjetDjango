cbee1fef2760a4dae785a3d3f0810851253e1f57
birametgod <birametgod95@gmail.com>
2018-04-01 18:23:12 +0000

    connexion inscription deconnexion  patient

--- PharmaLiv/connexion/urls.py
+++ PharmaLiv/connexion/urls.py
@@ -3,6 +3,10 @@ from . import views
 from patient import views as patient_views
 from django.contrib.auth import views as auth_views
 
+"""
+from django.contrib.auth import views => classe de django qui nous donne des methode
+pour nous permettre de gerer plus facilement la connexion et la deconnexion ...
+"""
 
 urlpatterns = [
     path('', views.home), 
@@ -10,7 +14,7 @@ urlpatterns = [
         path('', patient_views.home),
         path('inscription/', patient_views.connexionView.as_view()),
         path('thanks/', patient_views.reponse,name="afficher_reponse"),
-        path('logout/', auth_views.logout,{'template_name':'patient/logout.html'}),
-        path('login/', auth_views.login,{'template_name':'patient/login.html'}), 
+        path('logout/', auth_views.logout,{'template_name':'patient/logout.html'}),#.logout nous gere la deconnexion , meme pas besoin d'ecrire une methode dans views.py,il nous redirige directement dans le template_name
+        path('login/', auth_views.login,{'template_name':'patient/login.html'}), #meme chose pour .login ,patient/login.html est la page pour se connecter , lorsqu'on django nous gere la verification et nous redirige vers l'url indiqué dans input type hidden de la page html
     ])),
 ]
